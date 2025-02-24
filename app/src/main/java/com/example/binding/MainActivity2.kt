1185dce2a23f5ae8d8b7233974ca079364d07790
Arslan388 <arsl77220@gmail.com>
2025-02-24 15:38:00 +0500

    first commit

---
+++ app/src/main/java/com/example/binding/MainActivity2.kt
@@ -0,0 +1,19 @@
+package com.example.binding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.binding.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding:ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding=ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
